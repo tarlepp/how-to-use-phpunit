c2d0f45975f69ce1b1cc125c132fcb08fb6b7456
tarlepp <tarmo.leppanen@protacon.com>
2018-03-01 21:08:30 +0200

    fixed tests - second time - hopefully now for correct branch :D :töhötys:

--- tests/Functional/ProtaconGitHubTest.php
+++ tests/Functional/ProtaconGitHubTest.php
@@ -72,7 +72,6 @@ class ProtaconGitHubTest extends \PHPUnit_Extensions_Selenium2TestCase
                     'iot-hackday-r2d2',
                     'hackday-2015',
                     'hackday-2016-haksu',
-                    'iot-hackday-2015',
                     'hackday-2017-pappi',
                     'hackday-2017-autonomous-car',
                     'iot-hackday-iotkukka',
